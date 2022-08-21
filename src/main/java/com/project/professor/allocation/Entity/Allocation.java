f3966050ee9a786b0fb7ebf30297ad650f3903d2
José Carlos <josecarlospaivasantos2001@gmail.com>
2022-08-21 20:39:58 -0300

    Finalizando atv Camada de Modelo

--- src/main/java/com/project/professor/allocation/Entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/Entity/Allocation.java
@@ -3,14 +3,51 @@ package com.project.professor.allocation.Entity;
 import java.time.DayOfWeek;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Allocation {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Enumerated(value = EnumType.STRING)
+	@Column(name = "day", nullable = false)
 	private DayOfWeek day;
+
+	@Column(name = "start", nullable = false)
+	@Temporal(value = TemporalType.TIME)
 	private Date start;
+
+	@Column(name = "end", nullable = false)
+	@Temporal(value = TemporalType.TIME)
 	private Date end;
+
+	@Column(name = "course_id", nullable = false)
 	private Long courseId;
+
+	@Column(name = "professor_id", nullable = false)
 	private Long professorId;
+
+	@ManyToOne
+	@JoinColumn(name = "professor_id", nullable = false, insertable = false, updatable = false)
+	private Professor professor;
+
+	@ManyToOne
+	@JoinColumn(name = "course_id", nullable = false, insertable = false, updatable = false)
+	private Course course;
+
 	public Allocation(Long id, DayOfWeek day, Date start, Date end, Long courseId, Long professorId) {
 		super();
 		this.id = id;
@@ -19,13 +56,81 @@ public class Allocation {
 		this.end = end;
 		this.courseId = courseId;
 		this.professorId = professorId;
-		
-			
-		}
-public Allocation() {
-		
+
 	}
+
+	public Allocation() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+	public void setDay(DayOfWeek day) {
+		this.day = day;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public Long getProfessorId() {
+		return professorId;
+	}
+
+	public void setProfessorId(Long professorId) {
+		this.professorId = professorId;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "Allocation [id=" + id + ", day=" + day + ", start=" + start + ", end=" + end + ", courseId=" + courseId
+				+ ", professorId=" + professorId + ", professor=" + professor + ", course=" + course + "]";
+	}
+
 }
-
-	
-
--- src/main/java/com/project/professor/allocation/Entity/Professor.java
+++ src/main/java/com/project/professor/allocation/Entity/Professor.java
@@ -1,21 +1,82 @@
 package com.project.professor.allocation.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Professor {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "name", nullable = false)
 	private String name;
+
+	@Column(name = "cpf", unique = true, nullable = false, length = 11)
 	private String cpf;
+
+	@Column(name = "department_id", nullable = false)
 	private Long departmentId;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id", nullable = false, insertable = false, updatable = false)
+	private Departament departament;
+
 	public Professor(Long id, String name, String cpf, Long departmentId) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.departmentId = departmentId;
 	}
-public Professor () {
-		
+
+	public Professor() {
+		super();
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
 }
-
-
